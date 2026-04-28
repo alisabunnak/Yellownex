b03a3d8199b9ddc931bbca4b16be097516a9fb35
poomichopoom-p <Poomicho.p@gmail.com>
2026-04-28 15:20:41 +0700

    Version02

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,23 @@
-
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
-
 
 import Layout from "../../../react-app-starter/src/components/Layout";
 
 const router = createBrowserRouter([
   {
-    path:"/",
+    path: "/",
     element: <Layout />,
-    children:[
-      {
-
-      },{},{}
-    ]
+    children: [{}, {}, {}],
   },
-  
-])
-
+]);
 
 export default function App() {
-  
-
-  return <RouterProvider router={router} />; 
+  return <RouterProvider router={router} />;
 }
 
-<<<<<<< HEAD
-
-=======
 function MainFeed() {
   return (
     <div className="middle-column">
       <StartPost />
-
     </div>
-  )
+  );
 }
-
-export default App
->>>>>>> df57ef7a6678b1339d37d47535acb43d1d63f841
